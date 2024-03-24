85b50b5efde35646f6cfbc070b98f193e652da6c
shahafSegal <goigadol360@gmail.com>
2024-03-24 16:58:49 +0200

    basic build for server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const mongoose  = require('mongoose')
+const {app}=require('./app')
+const {config}=require('./config')
+
+mongoose.connect(config.MONGO_URL)
+    .then(()=>{
+        console.log("connected to db")
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+const PORT=process.env.PORT||4200
+
+
+app.listen(PORT,()=>{
+    console.log(`server running: PORT ${PORT}`)
+    }
+)
