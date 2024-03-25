0e2b8900e0add9ee23ccb3aadbbf41a23e42c86a
Jonathan Hazan <yonatanhazan1337@gmail.com>
2024-03-25 12:29:32 +0200

    Added CRUD

--- server/routes/company.routes.js
+++ server/routes/company.routes.js
@@ -1,9 +1,13 @@
 const {Router} = require('express')
-const {Register, Login} = require('../controllers/company.controller')
+const {Register, Login, getCompanies, deleteCompany} = require('../controllers/company.controller')
 const router = Router()
+
+router.get('/',getCompanies)
 
 router.post('/register', Register)
 router.post('/login',Login)
+
+router.delete('/:companyId',deleteCompany)
 
 
 module.exports = router
