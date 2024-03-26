1444e77cf0ad2d78aaab7ff8ef5fca1eb6baad5b
Ohad-Raz <ohadraz12@gmail.com>
2024-03-26 15:49:17 +0200

    select in feed with style

--- client/src/pages/Feed/Feed.jsx
+++ client/src/pages/Feed/Feed.jsx
@@ -7,10 +7,8 @@ import { useLocation } from "react-router-dom";
 
 function Feed() {
   const [formData, setFormData] = useState({});
-  const [filters, setfilters] = useState();
   const [volunteerJobs, setVolunteerJobs] = useState([]);
   let location = useLocation();
-  console.log(location.state.volunteerData);
 
   useEffect(() => {}, []);
 
@@ -18,6 +16,7 @@ function Feed() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     console.log(formData);
   };
+
   const getVolunteerByFilter = async () => {
     try {
       const res = await axios.post(
@@ -36,37 +35,33 @@ function Feed() {
       console.log(error);
     }
   };
+
   return (
     <div className={styles.feedContainer}>
       <div className={styles.containerOne}>
-        <h2>Find volunteering</h2>
+        <h2>Find volunteering opportunities</h2>
         <div className={styles.selectContainer}>
           <select name="area" onChange={changeHandler}>
-            <option value="">The volunteer area</option>
+            <option value="" disabled selected>
+              The volunteer area
+            </option>
             <option value="north">North</option>
             <option value="west">West</option>
             <option value="east">East</option>
             <option value="south">South</option>
           </select>
+        
           <select name="category" onChange={changeHandler}>
-            <option value="">{"Who would you like to help? (All)"}</option>
+            <option value="" disabled selected>
+              {"Who would you like to help? (All)"}
+            </option>
             <option value="Environment">Environment</option>
-            <option value="at risk youth">at risk youth</option>
-            <option value="elderly people">elderly people</option>
+            <option value="at risk youth">At-risk Youth</option>
+            <option value="elderly people">Elderly People</option>
             <option value="Building">Building</option>
-            <option value="agriculture">agriculture</option>
-            <option value="education">education</option>
-            <option value="software">software</option>
-          </select>
-          <select name="category" onChange={changeHandler}>
-            <option value="">{"Who would you like to help? (All)"}</option>
-            <option value="Environment">Environment</option>
-            <option value="at risk youth">at risk youth</option>
-            <option value="elderly people">elderly people</option>
-            <option value="Building">Building</option>
-            <option value="agriculture">agriculture</option>
-            <option value="education">education</option>
-            <option value="software">software</option>
+            <option value="agriculture">Agriculture</option>
+            <option value="education">Education</option>
+            <option value="software">Software</option>
           </select>
           <input
             type="text"
@@ -81,7 +76,9 @@ function Feed() {
             onChange={changeHandler}
           />
         </div>
-        <button onClick={getVolunteerByFilter}>Search</button>
+        <button className={styles.searchBtn} onClick={getVolunteerByFilter}>
+          Search
+        </button>
       </div>
       <div className={styles.cardsContainer}>
         <VolunteerCard
